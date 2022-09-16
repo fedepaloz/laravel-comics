e00ceba712b64432a173dc56a24793b0ac683c19
fedepaloz <palozzifederico@gmail.com>
2022-09-16 20:35:29 +0200

    comic artists added

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -8,8 +8,25 @@
     <h2>{{$comic['price']}}</h2>
     <h2>{{$comic['series']}}</h2>
     <h2>{{$comic['type']}}</h2>
+   
+    <div>
+
+
+
+         
+<ul>
+    @foreach ($comic['artists'] as $artists)
+        <li>
+<h1>{{$artists}}</h1>
+        </li>
+    @endforeach
+</ul>
+        
+    </div>
 </div>
 
 
 </div> 
 @endsection 
+
+<li></li>
